d205bd696ba1529c8fdd8e20244fc9857160bae1
Tran Ngoc Duc <caothu91@gmail.com>
2023-09-12 08:30:29 +0700

    add Ddns to model

--- Vm.php
+++ Vm.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Process;
 use Diepxuan\System\OperatingSystem as Os;
+use Diepxuan\System\Ddns;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
