61191f11462605a62110029c37905382bcbed23e
Tran Ngoc Duc <caothu91@gmail.com>
2023-09-15 11:24:36 +0700

    MANAGER-28 move ConfigServerSecurityFirewall to OS odel

--- ConfigServerSecurityFirewall.php
+++ ConfigServerSecurityFirewall.php
@@ -7,62 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Diepxuan\System\OperatingSystem\ConfigServerSecurityFirewall as CSF;
 
-class ConfigServerSecurityFirewall extends Model
+class ConfigServerSecurityFirewall extends CSF
 {
-    use \Diepxuan\System\ConfigServerSecurityFirewall\Config;
-    use \Diepxuan\System\ConfigServerSecurityFirewall\Cluster;
-    use \Diepxuan\System\ConfigServerSecurityFirewall\Port;
-    use \Diepxuan\System\ConfigServerSecurityFirewall\CsfPost;
-
-    public function isInstall(): Attribute
-    {
-        return Attribute::make(
-            get: fn (mixed $value, array $attributes) => self::isInstalled(),
-        );
-    }
-
-    public static function isInstalled(): bool
-    {
-        return Str::of(Process::run('command -v csf')->output())->isNotEmpty();
-    }
-
-    public static function getVersion()
-    {
-        return Str::of(preg_replace('/csf: ([\w\d]+)/i', '$1', Process::run("sudo csf -v | grep csf:")->output()))->trim();
-    }
-
-    public function version(): Attribute
-    {
-        return Attribute::make(
-            get: fn (mixed $value, array $attributes) => self::getVersion(),
-        );
-    }
-
-    /**
-     * Fix missing iptables default path for csf cmd
-     */
-    function iptables(): void
-    {
-        $this->_iptables('iptables');
-        $this->_iptables('iptables-save');
-        $this->_iptables('iptables-restore');
-    }
-
-    function _iptables($command): void
-    {
-        $cmdPath    = Process::run("command -v $command")->output();
-        $cmdDefault = "/sbin/$command";
-        if ($cmdPath == $cmdDefault) return;
-        if (!File::isFile($cmdDefault))
-            Process::run("sudo ln $(which $command) /sbin/$command");
-    }
-
-    public static function apply()
-    {
-        $flag = false;
-        $flag = $flag ?: self::rebuildConfiguration();
-        $flag = $flag ?: self::rebuildIptablesRules();
-        if ($flag) return Process::run("sudo csf -ra")->output();
-    }
 }
---
+++ OperatingSystem/ConfigServerSecurityFirewall.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Diepxuan\System\OperatingSystem;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class ConfigServerSecurityFirewall extends Model
+{
+    use \Diepxuan\System\ConfigServerSecurityFirewall\Config;
+    use \Diepxuan\System\ConfigServerSecurityFirewall\Cluster;
+    use \Diepxuan\System\ConfigServerSecurityFirewall\Port;
+    use \Diepxuan\System\ConfigServerSecurityFirewall\CsfPost;
+
+    public function isInstall(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => self::isInstalled(),
+        );
+    }
+
+    public static function isInstalled(): bool
+    {
+        return Str::of(Process::run('command -v csf')->output())->isNotEmpty();
+    }
+
+    public static function getVersion()
+    {
+        return Str::of(preg_replace('/csf: ([\w\d]+)/i', '$1', Process::run("sudo csf -v | grep csf:")->output()))->trim();
+    }
+
+    public function version(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => self::getVersion(),
+        );
+    }
+
+    /**
+     * Fix missing iptables default path for csf cmd
+     */
+    function iptables(): void
+    {
+        $this->_iptables('iptables');
+        $this->_iptables('iptables-save');
+        $this->_iptables('iptables-restore');
+    }
+
+    function _iptables($command): void
+    {
+        $cmdPath    = Process::run("command -v $command")->output();
+        $cmdDefault = "/sbin/$command";
+        if ($cmdPath == $cmdDefault) return;
+        if (!File::isFile($cmdDefault))
+            Process::run("sudo ln $(which $command) /sbin/$command");
+    }
+
+    public static function apply()
+    {
+        $flag = false;
+        $flag = $flag ?: self::rebuildConfiguration();
+        $flag = $flag ?: self::rebuildIptablesRules();
+        if ($flag) return Process::run("sudo csf -ra")->output();
+    }
+}
